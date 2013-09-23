b16495bf610cf60fef6becec0b77d54fa50ff414
Espen Moe-Nilssen <espen@medialog.no>
2013-09-23 21:57:56 +0200

    support for dexterity scales, but not

--- medialog/imageexport/browser/imageexport.py
+++ medialog/imageexport/browser/imageexport.py
@@ -12,7 +12,7 @@ from tempfile import TemporaryFile
 from plone.dexterity.utils import iterSchemata, resolveDottedName
 from zope.schema import getFields
 from plone.dexterity.interfaces import IDexterityContent
-from plone.namedfile.interfaces import INamedBlobImageField, IBlobField
+from plone.namedfile.interfaces import IImageScaleTraversable, INamedImageField
 
 class Exporter(BrowserView):
     
@@ -32,7 +32,6 @@ class Exporter(BrowserView):
 
         for obj in self.context.getFolderContents():
             obj = obj.getObject()
-
             try:
                 #this is for archetype
                 #imageformat is image/jpg so we are skipping the first part
@@ -48,20 +47,20 @@ class Exporter(BrowserView):
                 img = obj.Schema().getField('image').getScale(obj,scale=imagesize)
                 ZIP.writestr(self.context.getId() + '/' + full_image_name, str(img.data))
             except:
-                #this is for dexterity
+                #this is for dexterity blob fields
                 if IDexterityContent.providedBy(obj):
-                    schema = getFieldsInOrder(obj.getTypeInfo().lookupSchema()
                     for schemata in iterSchemata(obj):
                         for name, field in getFields(schemata).items():
-                            if IBlobField.providedBy(obj):
-                                full_image_name = str(name)
+                            #checking if it is an image field
+                            if INamedImageField.providedBy(field):
+                                #need to get file name, not field name here.
                                 import pdb; pdb.set_trace()
-                                value = (getattr(obj, name)).data
-                                ZIP.writestr(self.context.getId() + '/' + full_image_name, str(object.data))
-            else:
-                #this is just for the vikings
-                full_image_name = str(obj.visningsbilde.filename)
-                ZIP.writestr(self.context.getId() + '/' + full_image_name, str(obj.icon.data))
+                                #full_image_name = str(field.filename)
+                                #copied this line from somewhere
+                                field_value = field.get(field.interface(obj))
+                                if field_value is not None:
+                                    #field_value is not correct, it is the image, not the scale
+                                    ZIP.writestr(self.context.getId() + '/' + full_image_name, str(field_value.data))
             finally:
                 pass
         
